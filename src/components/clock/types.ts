42b9a2338b36509829447b6ab54af8d6a61256dd
Ahaz Fernando <ahazfernando@gmail.com>
2025-11-13 17:49:37 +0530

    Refactor

--- src/components/clock/types.ts
+++ src/components/clock/types.ts
@@ -53,3 +53,4 @@ export const getTimeEntryData = (data: unknown): FirestoreTimeEntry => {
 };
 
 
+
--- src/components/clock/utils.ts
+++ src/components/clock/utils.ts
@@ -63,3 +63,4 @@ export const mergeEntriesByUserAndDate = (entries: TimeEntry[]): MergedTimeEntry
 };
 
 
+
