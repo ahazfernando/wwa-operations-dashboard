778acc9425b931618e24d2f30e8c3f7c0af87d72
LGCDMBandara <dchathura509@gmail.com>
2026-01-29 09:40:59 +0530

    google-api-implement

---
+++ src/app/google-api/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import dynamic from 'next/dynamic';
+
+// Dynamically import with SSR disabled to prevent build-time errors with AuthProvider
+const ProtectedRoute = dynamic(() => import("@/components/ProtectedRoute").then(mod => ({ default: mod.ProtectedRoute })), {
+  ssr: false,
+});
+
+const DashboardLayout = dynamic(() => import("@/components/DashboardLayout").then(mod => ({ default: mod.DashboardLayout })), {
+  ssr: false,
+});
+
+const GoogleAPI = dynamic(() => import("@/pages/GoogleAPI"), {
+  ssr: false,
+});
+
+export default function GoogleAPIPage() {
+  return (
+    <ProtectedRoute>
+      <DashboardLayout>
+        <GoogleAPI />
+      </DashboardLayout>
+    </ProtectedRoute>
+  );
+}
+
